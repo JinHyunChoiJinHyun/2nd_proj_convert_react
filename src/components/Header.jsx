98b9eb26569c33a7791c3be1693af357eeed1b9d
choi <chlwlsgus543@gmail.com>
2025-05-21 17:53:12 +0900

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <h3>매수하기 딱 좋은 날씨네</h3>
+          <ul>
+            <li>메인</li>
+            <li>수익률 예측</li>
+            <li>종목 토론</li>
+          </ul>
+          <form action="">
+            <input type="text" />
+            <button>검색</button>
+            <button>로그인</button>
+          </form>
+
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
